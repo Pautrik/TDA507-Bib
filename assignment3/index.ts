07f60c32145302789853aaee5081b891fe086d3d
pautrik <>
2019-12-03 09:30:57 +0100

    Rewrote to typescript and fixed bug where chain could become infinitely cyclical

--- assignment3/index.ts
+++ assignment3/index.ts
@@ -1,6 +1,17 @@
-const fs = require('fs');
+import { readFileSync } from 'fs';
 
-function parseContent(str) {
+interface Atom {
+    id: number;
+    coords: Coords;
+}
+
+interface Coords {
+    x: number;
+    y: number;
+    z: number;
+}
+
+function parseContent(str: string) : Atom[] {
     return str.split('\n').map(line => {
         const strParts = line.match(/\S+/g);
         const parts = strParts.map(parseFloat);
@@ -15,17 +26,18 @@ function parseContent(str) {
     });
 }
 
-function findChain(atom, previous, atoms, chain = '') {
+function findChain(atom: Atom, atoms: Atom[], chain = ''): string {
     let bestChain = '';
 
-    for(let next of atoms) {
-        if(atom.id === next.id || (previous && previous.id === next.id))
-            continue;
+    for(let i = 0; i < atoms.length; i++) {
+
+        const atomsCopy = [...atoms]; // clone array
+        const next = atomsCopy.splice(i, 1)[0];
 
         const d = distance(atom.coords, next.coords);
 
         if(3.6 < d && d < 4.0) {
-            const partChain = findChain(next, atom, atoms, chain);
+            const partChain = findChain(next, atomsCopy, chain);
 
             if(partChain.length > bestChain.length) {
                 bestChain = partChain;
@@ -36,7 +48,7 @@ function findChain(atom, previous, atoms, chain = '') {
     return `${atom.id},${bestChain}`;
 }
 
-function distance(a, b) {
+function distance(a: Coords, b: Coords): number {
     return Math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2 + (a.z - b.z)**2);
 }
 
@@ -49,14 +61,16 @@ function main() {
 
     let bestChain = '';
     
-    const fileContent = fs.readFileSync(process.argv[2], 'utf-8');
+    const fileContent= readFileSync(process.argv[2], 'utf-8');
     const atoms = parseContent(fileContent.trim());
 
-    for(let atom of atoms) {
-        const testChain = findChain(atom, null, atoms, atom.id);
+    for(let i = 0; i < atoms.length; i++) {
+        const atomsCopy = [...atoms];
+        const atom = atomsCopy.splice(i, 1)[0];
+        const testChain = findChain(atom, atomsCopy, atom.id.toString());
 
         if(bestChain.length < testChain.length) {
-            bestChain = testChain;
+            bestChain = testChain;  
         }
     }
 
